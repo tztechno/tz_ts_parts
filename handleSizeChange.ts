4279ed148243b6b3c9e2e3440163f9cde0f09e1d
stpeteishii <47345279+tztechno@users.noreply.github.com>
2024-07-28 11:34:11 +0900

    Update handleSizeChange.ts

--- handleSizeChange.ts
+++ handleSizeChange.ts
@@ -5,3 +5,16 @@ const handleSizeChange = (newSize: number) => {
         setStepNumber(0);
         setXIsNext(true);
     };
+
+--------------------------------------------------------------------------------
+        
+                <div>
+                    <label>
+                        Board Size:
+                        <select value={boardSize} onChange={(e) => handleSizeChange(Number(e.target.value))}>
+                            <option value="3">3x3</option>
+                            <option value="4">4x4</option>
+                            <option value="5">5x5</option>
+                        </select>
+                    </label>
+                </div>
